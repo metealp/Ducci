ba57d27ca11b9a5e1e02d4a67bdd8ccf27e920d2
Mete Alp Kızılçay <kizilcaymetealp@gmail.com>
2020-10-13 21:08:28 +0300

    Initial Commit

---
+++ Ducci/DucciApp.swift
@@ -0,0 +1,17 @@
+//
+//  DucciApp.swift
+//  Ducci
+//
+//  Created by Mete Alp Kizilcay on 13.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct DucciApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
